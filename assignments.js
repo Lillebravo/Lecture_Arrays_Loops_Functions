1b6c5721f55bf2b7367c2c14ce4f8c7eb4089d9c
Jerry <you@example.com>
2024-10-29 12:49:48 +0100

    uppgift 6

--- assignments.js
+++ assignments.js
@@ -1,7 +1,7 @@
 // --- 1. Utgå ifrån array nedanför. ---
 // Skriv ut namnet på index-position 3. Vilket namn skrivs ut och varför?
 
-const names = ["Zlatan", "Beckham", "Zidane", "Maradona", "Pele", "Ronaldo"];
+let names = ["Zlatan", "Beckham", "Zidane", "Maradona", "Pele", "Ronaldo"];
 console.log(names[3]); 
 // skriver ut "Maradona", eftersom arrayens index börjar på 0 skrivs 4e positionen ut
 
@@ -9,4 +9,32 @@ console.log(names[3]);
 names.push("Drogba");
 // Svar: den returnerar 7
 
+// --- 3. Lägg nu till namnet "Figo" i börja av arrayen. 
+//Vilken metod använder du för att göra det och vad är returnvärdet av den metoden?
+names.unshift("Figo");
+// Svar: den returnerar 8
+
+// --- 4. Ta nu bort det sista namnet från array med pop(). Logga ut returvärdet av den metoden. ---
+let lastName = names.pop();
+console.log(lastName);
+
+// --- 5. Vid det här laget så bör du ha motsvarande array nedan. Hur kan du flytta namnet "Ronaldo" från sluten av arrayen
+// till början av arrayen? Visa i kod samt förklara varför du gör som du gör. ---
+names = [
+    "Figo",
+    "Zlatan",
+    "Beckham",
+    "Zidane",
+    "Maradona",
+    "Pele",
+    "Ronaldo",
+  ];
+  names.splice(0, 0, "Ronaldo"); // Skapar en kopia av ronaldo och sätter det på position 0
+  names.pop(); // Tar bort Ronaldo som låg i slutet av arrayen
+
+  console.log(names);
+
+// --- 6. Vid det här laget hoppas jag att du märkt att de metoderna som du har använt påverkar ursprungs-arrayn,
+//alltså den modifierar den befintliga arrayen. Detta kallas för att en metod är "destruktiv". Så även fast vi definierar
+//arrayen med en const-variabel så tillåts vi ändå att modifiera arrayn. Vad kan detta bero på?
 
