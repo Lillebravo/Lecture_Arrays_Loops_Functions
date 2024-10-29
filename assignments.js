847335754631d700449dc4bd12bb8bba80dd0eea
Jerry <you@example.com>
2024-10-29 12:36:18 +0100

    added html and js files for assignments

---
+++ assignments.js
@@ -0,0 +1,12 @@
+// --- 1. Utgå ifrån array nedanför. ---
+// Skriv ut namnet på index-position 3. Vilket namn skrivs ut och varför?
+
+const names = ["Zlatan", "Beckham", "Zidane", "Maradona", "Pele", "Ronaldo"];
+console.log(names[3]); 
+// skriver ut "Maradona", eftersom arrayens index börjar på 0 skrivs 4e positionen ut
+
+// --- 2. I samma array, lägg till namnet "Drogba" i slutet av arrayen med hjälp av push(). Vad är returvärdet av den metoden? ---
+names.push("Drogba");
+// Svar: den returnerar 7
+
+
